5cee82244e5c80484fc262d71f8c2721e6a109a4
Oleksiy Kotylev <okotylev@galiciaseguros.com.ar>
2022-07-07 11:03:11 -0300

    modo clueste con la variable

--- src/app.js
+++ src/app.js
@@ -6,34 +6,54 @@ import {cartRouter} from './routers/cart-router.js';
 import { validateAdmin } from './middleware/middleware.js';
 import {mongooseCon} from "./config/configurations.js";
 import {userRouter} from "./routers/user-router.js";
+import cluster from 'cluster';
+import os from 'os';
 
 
+const totalCpus = os.cpus().length;
 
-await mongooseCon();
+const enableCluster = process.env.ENABLECLUSTER === 'true'
 
-const app = express();
+if (cluster.isMaster && enableCluster) {
+  console.log(`Master ${process.pid} is running`);
 
-const multerMid = multer({
-  storage: multer.memoryStorage(),
-  limits: {
-    fileSize: 5 * 1024 * 1024,
-  },
-});
+  for (let i = 0; i < totalCpus; i++) {
+    cluster.fork();
+  }
 
-const port = 8080;
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
 
-process.env.DAO = 'firebase';
+  await mongooseCon();
 
-app.listen(process.env.PORT || port, () => {
-  console.log(`Example app listening on port ${port}`);
-  console.log(process.env.DAO);
-});
+  const app = express();
+
+  const multerMid = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+      fileSize: 5 * 1024 * 1024,
+    },
+  });
+
+  const port = 8080;
+
+  process.env.DAO = 'firebase';
+
+  app.listen(process.env.PORT || port, () => {
+    console.log(`Example app listening on port ${port}`);
+    console.log(process.env.DAO);
+  });
 
 
-app.use(multerMid.single('file'))
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(validateAdmin);
-app.use('/api/products', productRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/users', userRouter);
+  app.use(multerMid.single('file'))
+  app.use(express.json());
+  app.use(express.urlencoded({extended: true}));
+  app.use(validateAdmin);
+  app.use('/api/products', productRouter);
+  app.use('/api/cart', cartRouter);
+  app.use('/api/users', userRouter);
+
+}
